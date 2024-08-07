4b8fe7b9a9bc6391ff0c8cfc7c7aeb4b65a96fb4
Natu <nf231105@gmail.com>
2024-08-07 10:24:30 -0300

    Implementación de la operación Multiplicacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+// Esta clase implementa la operación Multiplicacion>
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
